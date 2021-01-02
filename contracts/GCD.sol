ba714c1e00ee2dacc39a300702895fc213c66a00
minami <minaminaoy@gmail.com>
2021-01-03 04:30:06 +0900

    Add doc comments

--- contracts/GCD.sol
+++ contracts/GCD.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0;
 
+/** @title Greatest Common Divisor */
 library GCD {
+    /// @dev gcd with recursion
+    /// @param x integer
+    /// @param y integer
+    /// @return gcd
     function gcd(uint256 x, uint256 y) internal pure returns (uint256) {
         if (y > 0) {
             return gcd(y, x % y);
@@ -10,6 +15,10 @@ library GCD {
         }
     }
 
+    /// @dev gcd without recursion
+    /// @param x integer
+    /// @param y integer
+    /// @return gcd
     function gcd_loop(uint256 x, uint256 y) internal pure returns (uint256) {
         uint256 t;
         while (y > 0) {
